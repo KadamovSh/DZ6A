3384c5bfb4565c731d35b30fe96f141b5612c8ff
MrShoir <kadamov.shoir24@mail.ru>
2026-02-19 19:29:09 +0500

    Добавлен card, types

---
+++ pkg/bank/card/card.go
@@ -0,0 +1,15 @@
+package card
+
+import "github.com/KadamovSh/DZ6A/pkg/bank/types"
+
+// IssueCard создаёт карту
+func IssueCard(currency types.Currency, color string, name string) types.Card {
+    // TODO: написать код
+    return types.Card{}
+}
+
+// Withdraw снимает деньги с карты
+func Withdraw(card types.Card, amount types.Money) types.Card {
+    // TODO: написать код
+    return card
+}
